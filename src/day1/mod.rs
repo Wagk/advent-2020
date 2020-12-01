1e3c097f561e854314913f6f60fc0d00a146cc99
Pang Tun Jiang <mail@pangt.dev>
2020-12-01 23:41:11 +0800

    Implement day 1 of advent of code

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,8 +1,36 @@
-fn part1() {
-    let input = include_str!("input");
-    todo!();
+use itertools::Itertools;
+
+pub fn part1() -> Option<usize> {
+    let input = include_str!("input")
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    if let Some((&x, &y)) = input
+        .iter()
+        .cartesian_product(input.iter())
+        .find(|(&x, &y)| x + y == 2020)
+    {
+        Some(x * y as usize)
+    } else {
+        None
+    }
 }
 
-fn part2() {
-    todo!();
+pub fn part2() -> Option<usize> {
+    let input = include_str!("input")
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    if let Some(((&x, &y), &z)) = input
+        .iter()
+        .cartesian_product(input.iter())
+        .cartesian_product(input.iter())
+        .find(|((&x, &y), &z)| x + y + z == 2020)
+    {
+        Some(x * y * z as usize)
+    } else {
+        None
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{} day1 part1", day1::part1().unwrap());
+    println!("{} day1 part2", day1::part2().unwrap());
 }
