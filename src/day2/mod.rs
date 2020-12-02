378b60a7696e94dcb72a3955e3c5b3b4541be642
Pang Tun Jiang <mail@pangt.dev>
2020-12-02 19:13:07 +0800

    Implemented day 2 part 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -26,3 +26,37 @@ pub fn part1() -> usize {
         })
         .count()
 }
+
+pub fn part2() -> usize {
+    let input = include_str!("input").lines().collect::<Vec<_>>();
+
+    input
+        .into_iter()
+        .filter(|password| {
+            match password
+                .split_whitespace()
+                .collect::<Vec<&str>>()
+                .as_slice()
+            {
+                [range, letter, password] => {
+                    let (min, max) = {
+                        let range = range
+                            .split("-")
+                            .map(|s| s.parse::<usize>().expect("minmax parsing has failed"))
+                            .collect::<Vec<_>>();
+                        (range[0], range[1])
+                    };
+                    let letter = letter
+                        .strip_suffix(":")
+                        .expect("trailing colon has failed")
+                        .parse::<char>()
+                        .expect("char parsing has failed");
+
+                    (password.chars().nth(min - 1).unwrap() == letter)
+                        ^ (password.chars().nth(max - 1).unwrap() == letter)
+                }
+                _ => panic!(),
+            }
+        })
+        .count()
+}
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ fn main() {
     // println!("{} day1 part1", day1::part1().unwrap());
     // println!("{} day1 part2", day1::part2().unwrap());
     println!("{} day2 part1", day2::part1());
+    println!("{} day2 part2", day2::part2());
 }
