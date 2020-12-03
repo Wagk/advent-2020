90f70994a8bb135d99274b9ec0f05d4db3acb749
Pang Tun Jiang <mail@pangt.dev>
2020-12-03 23:47:02 +0800

    Stubbing out day3 part 1, since I started late.

---
+++ src/day3/mod.rs
@@ -0,0 +1,13 @@
+struct Map {
+    layout: Vec<Vec<char>>,
+}
+
+impl Map {
+    fn load_map(map: &str) -> Self {}
+
+    // TODO: add indexing and all that here.
+}
+
+pub fn part1() -> usize {
+    let input = include_str!("input").lines().collect::<Vec<_>>();
+}
